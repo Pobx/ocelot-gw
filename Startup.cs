c72dba5e0335051356756d0af054d31ecd29a7b9
Pobx <geidtiphong@gmail.com>
2022-04-18 16:46:39 +0700

    remove comment

--- Startup.cs
+++ Startup.cs
@@ -37,31 +37,10 @@ namespace ocelot_gw
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-
-      // services.AddSwaggerForOcelot (Configuration, (options) => {
-      //   options.GenerateDocsForAggregates = true;
-      //   options.AggregateDocsGeneratorPostProcess = (aggregateRoute, routesDocs, pathItemDoc, documentation) => {
-      //     var apiGwPath = Configuration.GetValue<string> ("ApiGwPath");
-      //     var aggregateRouteUpstreamPathTemplate = $"{apiGwPath}{aggregateRoute.UpstreamPathTemplate}";
-
-      //     Console.WriteLine ($"aggregateRoute: {aggregateRoute}");
-      //     Console.WriteLine ($"routesDocs: {routesDocs}");
-      //     Console.WriteLine ($"pathItemDoc: {pathItemDoc}");
-      //     Console.WriteLine ($"documentation: {documentation}");
-
-      //     if (!aggregateRoute.UpstreamPathTemplate.Contains ("apigw")) {
-      //       aggregateRoute.UpstreamPathTemplate = aggregateRouteUpstreamPathTemplate;
-      //     }
-      //   };
-      // });
-
-      // services.AddOcelot(Configuration).AddAppConfiguration();
-
       services.AddControllers();
       services.AddOcelot().AddAppConfiguration();
       services.AddSwaggerForOcelot(Configuration);
 
-      // var authenticationProviderKey = "IdentityApiKey";
       var handler = new HttpClientHandler();
       handler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
 
@@ -100,36 +79,6 @@ namespace ocelot_gw
       });
 
       app.UseOcelot().Wait();
-
-      // var apiGWPath = Configuration.GetValue<string>("ApiGwPath");
-      // app.UseSwaggerForOcelotUI(opt =>
-      // {
-      //   opt.DownstreamSwaggerEndPointBasePath = $"{apiGWPath}/swagger/docs";
-      //   opt.ReConfigureUpstreamSwaggerJson = DefinedUpstreamTransformer;
-      // });
-      // app.UseOcelot().Wait();
-
-      // app.UseEndpoints (endpoints => {
-      //   endpoints.MapControllers ();
-      // });
-
     }
-
-    // private string DefinedUpstreamTransformer(HttpContext context, string openApiJson)
-    // {
-    //   var swagger = JObject.Parse(openApiJson);
-    //   JToken paths = swagger[OpenApiProperties.Paths];
-
-    //   var allpaths = paths.Values<JProperty>().ToList();
-    //   var apiGWPath = Configuration.GetValue<string>("ApiGwPath");
-    //   foreach (var item in allpaths)
-    //   {
-    //     Console.WriteLine($"{apiGWPath}" + item.Name, item.Value);
-    //     var newProperty = new JProperty($"{apiGWPath}" + item.Name, item.Value);
-    //     item.Replace(newProperty);
-    //   }
-
-    //   return swagger.ToString(Formatting.Indented);
-    // }
   }
 }

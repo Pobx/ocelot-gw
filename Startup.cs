86b5187f8c2209911260b2bd11df805a8d327573
Pobx <geidtiphong@gmail.com>
2022-04-18 21:48:25 +0700

    remove UseAuthorization component

--- Startup.cs
+++ Startup.cs
@@ -69,9 +69,7 @@ namespace ocelot_gw
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
-      // app.UseHttpsRedirection();
       app.UseRouting();
-      app.UseAuthorization();
 
       app.UseSwaggerForOcelotUI(options =>
       {
